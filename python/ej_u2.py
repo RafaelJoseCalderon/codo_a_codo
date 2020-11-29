71c21db2927c4d8e8e106384087881469d1ddf19
RafaelJose <rafael_jose_calderon@hotmail.com>
2020-11-29 19:51:44 -0300

    ejercicios hasta el 20

--- python/ej_u2.py
+++ python/ej_u2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from math import pi, sqrt, factorial
+from math import pi, sqrt, factorial, trunc
+from time import mktime, strptime, strftime
 
 def ejercicio_1(numero_1, numero_2):
     return numero_1 * numero_2
@@ -26,14 +27,14 @@ def circulo_area(radio):
 def volumen_esfera(radio):
     return 4 * pi * radio ** 3 / 3
 
-def hipotenusa_rectangulo(cMayor, cMenor):
-    return sqrt(cMayor ** 2 + cMenor ** 2)
+def hipotenusa_rectangulo(c_mayor, c_menor):
+    return sqrt(c_mayor ** 2 + c_menor ** 2)
 
-#ejercicio 4
+# ejercicio 4
 # parte 1
 #  >>> for i in range(5):
 #  ...     print(i*i)
-#  ... 
+#  ...
 #  0
 #  1
 #  4
@@ -43,7 +44,7 @@ def hipotenusa_rectangulo(cMayor, cMenor):
 # parte 2
 #  >>> for i in range(2, 6):
 #  ...     print(i, 2 ** i)
-#  ... 
+#  ...
 #  2 4
 #  3 8
 #  4 16
@@ -76,9 +77,9 @@ def tabla_de_multiplicar(n):
 
 def ejercicio_7():
     palabra = input("Escriba un palabra: ")
-    for i in range(1000):
+    for _ in range(1000):
         print(palabra, end=" ")
-    
+
     print("")
 
 # ejercicio 8
@@ -87,7 +88,7 @@ def paridad_1(numero):
         resultado = 1
     else:
         resultado = 0
-    
+
     return resultado
 
 def paridad_2(numero):
@@ -108,8 +109,122 @@ def pares_entre(numero_1, numero_2):
 
 # ejercicio 10
 def triangulares_1():
-    numero = int(input("Escriba el valor de los valores algulares que requiere: "))
+    numero = int(
+        input("Escriba el valor de los valores algulares que requiere: "))
     acumulador = 0
     for i in range(1, numero + 1):
         acumulador += i
         print(f'{i} - {acumulador}')
+
+def ejercicio_11():
+    for _ in range(3):
+        numero = int(input("ingrese un entero para calcular su factorial: "))
+        print(f'El resultado es {factorial_1(numero)}')
+
+# no entendi el enunciado, copio y pego
+def ejercicio_12():
+    for i in range(1, 7):
+        for j in range(i, 7):
+            print("{}:{}".format(i, j))
+
+def ejercicio_13(n):
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            print("{}:{}".format(i, j))
+
+# ejercicio 14
+def duracion_segundos(tiempo_0, tiempo_1):  # "20:40:30"
+    tiempo_0 = mktime(strptime(tiempo_0, "%H:%M:%S"))
+    tiempo_1 = mktime(strptime(tiempo_1, "%H:%M:%S"))
+    return tiempo_1 - tiempo_0
+
+def duracion_horas(tiempo_0, tiempo_1):  # "20:40:30"
+    return duracion_segundos(tiempo_0, tiempo_1) / 3600
+
+def ejercicio_15(tiempo_0, tiempo_1):  # "20:40:30"
+    segundos = trunc(duracion_segundos(tiempo_0, tiempo_1))
+    minutos = trunc(segundos / 60)
+    horas = trunc(duracion_horas(tiempo_0, tiempo_1))
+    return f'{horas} horas, {minutos} minutos, {segundos} segundos'
+
+def ejercicio_16(d):  # x(
+    f = ""
+    I = ""
+
+    for i in range(d):
+        f += "0 "
+
+    for i in range(d):
+        I += f[:2 * i] + "1" + f[2 * i + 1:] + "\n"
+
+    print(I)
+
+# ejercicio 17
+def anio_bisiesto(anio):
+    return anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0)
+
+def dias_por_anio_mes(mes, anio):
+    return deferencia_fechas(f'1/1/{anio}', f'1/{mes}/{anio}') // 86400
+
+def fecha_valida(dia, mes, anio):
+    try:
+        mktime(strptime(f'{dia}/{mes}/{anio}', "%d/%m/%Y"))
+        print("Fecha valida")
+    except ValueError:
+        print("Fecha invalida")
+
+def dias_a_fin_de_mes(fecha):
+    base = strptime(fecha, "%d/%m/%Y")
+    return deferencia_fechas(fecha, f'1/{base.tm_mon + 1}/{base.tm_year}') // 86400 - 1
+
+def dias_a_fin_de_anio(fecha):
+    base = strptime(fecha, "%d/%m/%Y")
+    return deferencia_fechas(fecha, f'1/1/{base.tm_year + 1}') // 86400 - 1
+
+def dias_trascurridos_hasta(fecha):
+    base = strptime(fecha, "%d/%m/%Y")
+    return 1 - deferencia_fechas(fecha, f'1/1/{base.tm_year}') // 86400
+
+def diferencia_365_30(fecha_1, fecha_2):
+    resto = deferencia_fechas(fecha_1, fecha_2)
+    (anios, resto) = divmod(resto, 31536000) # anios de 365 dias
+    (meses, resto) = divmod(resto, 2592000)  # meses de 30 dias
+    dias = resto // 86400
+    return {'anios': int(anios), 'meses': int(meses), 'dias': int(dias)}
+
+def deferencia_fechas(fecha_1, fecha_2):
+    fecha_1 = mktime(strptime(fecha_1, "%d/%m/%Y"))
+    fecha_2 = mktime(strptime(fecha_2, "%d/%m/%Y"))
+    return fecha_2 - fecha_1
+
+def ejercicio_18(dia): # 1996 anio bisiesto que empieza en lunes
+    dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+    return dias[(dia - 1) % 7]
+
+# copiado y pegado
+def ejercicio_19(decimal):
+    num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    rom = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    retorno = ""
+
+    if decimal > 0 and decimal <= 4000:
+        for i in range(12, -1, -1):
+            while decimal >= num[i]:
+                decimal -= num[i]
+                retorno += rom[i]
+    
+    return retorno
+
+def ejercicio_20():
+    signo  = ["capricornio", "acuario", "piscis", "aries", "tauro", "géminis", "cáncer", "leo", "virgo", "libra", "escorpio", "sagitario"]
+
+    dia = int(input("dia: "))
+    mes = int(input("mes: ")) - 1
+
+    if dia > 20:
+        mes += 1
+    
+    if mes == 12:
+        mes = 0
+
+    print ("Tu signo es: " + signo[mes])
--- python/test_ej_u2.py
+++ python/test_ej_u2.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 from math          import pi
 
 class TestEJU2(unittest.TestCase):
+
     def test_ejercicio_1(self):
         self.assertEqual(ej_u2.ejercicio_1(20, 30), 600)
 
@@ -133,6 +134,184 @@ class TestEJU2(unittest.TestCase):
     def test_ejercicio_10(self, mock_input, mock_stdout):
         ej_u2.triangulares_1()
         self.assertEqual(mock_stdout.getvalue(), "1 - 1\n2 - 3\n3 - 6\n4 - 10\n5 - 15\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["1", "2", "5"])
+    def test_ejercicio_11(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_11()
+        self.assertEqual(
+            mock_stdout.getvalue(),
+            "El resultado es 1\n"+
+            "El resultado es 2\n"+
+            "El resultado es 120\n"
+        )
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_ejercicio_12(self, mock_stdout):
+        ej_u2.ejercicio_12()
+        self.assertEqual(
+            mock_stdout.getvalue(),
+            "1:1\n1:2\n1:3\n1:4\n1:5\n1:6\n2:2\n2:3\n2:4\n2:5\n2:6\n"+
+            "3:3\n3:4\n3:5\n3:6\n4:4\n4:5\n4:6\n5:5\n5:6\n6:6\n"
+        )
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_ejercicio_13(self, mock_stdout):
+        ej_u2.ejercicio_13(6)
+        self.assertEqual(
+            mock_stdout.getvalue(),
+            "1:1\n1:2\n1:3\n1:4\n1:5\n1:6\n2:2\n2:3\n2:4\n2:5\n2:6\n"+
+            "3:3\n3:4\n3:5\n3:6\n4:4\n4:5\n4:6\n5:5\n5:6\n6:6\n"
+        )
+
+    def test_ejercicio_14_1(self):
+        hora_0 = "20:34:30"
+        hora_1 = "20:40:30"
+        self.assertAlmostEqual(ej_u2.duracion_segundos(hora_0, hora_1), 360.00, 2)
+
+    def test_ejercicio_14_2(self):
+        hora_0 = "20:34:30"
+        hora_1 = "20:40:30"
+        self.assertAlmostEqual(ej_u2.duracion_horas(hora_0, hora_1), 0.10, 2)
+
+    def test_ejercicio_15(self):
+        hora_0 = "20:34:30"
+        hora_1 = "20:40:30"
+        self.assertEqual(
+            ej_u2.ejercicio_15(hora_0, hora_1),
+            "0 horas, 6 minutos, 360 segundos"
+        )
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_ejercicio_16(self, mock_stdout):
+        ej_u2.ejercicio_16(5)
+        self.assertEqual(
+            mock_stdout.getvalue(),
+            "1 0 0 0 0 \n"+
+            "0 1 0 0 0 \n"+
+            "0 0 1 0 0 \n"+
+            "0 0 0 1 0 \n"+
+            "0 0 0 0 1 \n\n"
+        )
+
+    # ejercicio 17
+    def test_bisiesto(self):
+        self.assertEqual(ej_u2.anio_bisiesto(2000), True)
+        self.assertEqual(ej_u2.anio_bisiesto(2400), True)
+        self.assertEqual(ej_u2.anio_bisiesto(1900), False)
+        self.assertEqual(ej_u2.anio_bisiesto(2100), False)
+        self.assertEqual(ej_u2.anio_bisiesto(2300), False)
+
+    def test_dias_por_anio_mes(self):
+        self.assertEqual(ej_u2.dias_por_anio_mes(11, 2020), 305)
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_fecha_valida_1(self, mock_stdout):
+        ej_u2.fecha_valida(28, 7, 1983)
+        self.assertEqual(mock_stdout.getvalue(), "Fecha valida\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_fecha_valida_2(self, mock_stdout):
+        ej_u2.fecha_valida(55, 99, 1983)
+        self.assertEqual(mock_stdout.getvalue(), "Fecha invalida\n")
+
+    def test_dias_a_fin_de_mes(self):
+        fecha = "28/11/2020"
+        self.assertEqual(ej_u2.dias_a_fin_de_mes(fecha), 2)
+
+    def test_dias_a_fin_de_anio(self):
+        fecha = "28/11/2020"
+        self.assertEqual(ej_u2.dias_a_fin_de_anio(fecha), 33)
+
+    def test_dias_trascurridos_hasta(self):
+        fecha = "28/11/2020"
+        self.assertEqual(ej_u2.dias_trascurridos_hasta(fecha), 333)
+        # anio bisiesto
+
+    def test_diferencia_365_30(self):
+        self.assertEqual(
+            ej_u2.diferencia_365_30("28/11/2020", "15/06/2025"),
+            {'anios': 4, 'meses': 6, 'dias': 20}
+        )
+
+    def test_ejercicio_18(self):
+        self.assertEqual(ej_u2.ejercicio_18(1), "lunes")
+        self.assertEqual(ej_u2.ejercicio_18(9), "martes")
+
+    def test_ejercicio_19(self):
+        self.assertEqual(ej_u2.ejercicio_19(   3),    "III")
+        self.assertEqual(ej_u2.ejercicio_19(   4),     "IV")
+        self.assertEqual(ej_u2.ejercicio_19(   5),      "V")
+        self.assertEqual(ej_u2.ejercicio_19(   8),   "VIII")
+        self.assertEqual(ej_u2.ejercicio_19(   9),     "IX")
+        self.assertEqual(ej_u2.ejercicio_19(  10),      "X")
+        self.assertEqual(ej_u2.ejercicio_19(  15),     "XV")
+        self.assertEqual(ej_u2.ejercicio_19(  19),    "XIX")
+        self.assertEqual(ej_u2.ejercicio_19(  30),    "XXX")
+        self.assertEqual(ej_u2.ejercicio_19(  39),  "XXXIX")
+        self.assertEqual(ej_u2.ejercicio_19(  40),     "XL")
+        self.assertEqual(ej_u2.ejercicio_19(  41),    "XLI")
+        self.assertEqual(ej_u2.ejercicio_19(  45),    "XLV")
+        self.assertEqual(ej_u2.ejercicio_19(  49),   "XLIX")
+        self.assertEqual(ej_u2.ejercicio_19(  50),      "L")
+        self.assertEqual(ej_u2.ejercicio_19(  80),   "LXXX")
+        self.assertEqual(ej_u2.ejercicio_19(  90),     "XC")
+        self.assertEqual(ej_u2.ejercicio_19(  99),   "XCIX")
+        self.assertEqual(ej_u2.ejercicio_19( 100),      "C")
+        self.assertEqual(ej_u2.ejercicio_19( 150),     "CL")
+        self.assertEqual(ej_u2.ejercicio_19( 190),    "CXC")
+        self.assertEqual(ej_u2.ejercicio_19( 300),    "CCC")
+        self.assertEqual(ej_u2.ejercicio_19( 390),  "CCCXC")
+        self.assertEqual(ej_u2.ejercicio_19( 400),     "CD")
+        self.assertEqual(ej_u2.ejercicio_19( 410),    "CDX")
+        self.assertEqual(ej_u2.ejercicio_19( 450),    "CDL")
+        self.assertEqual(ej_u2.ejercicio_19( 495),  "CDXCV")
+        self.assertEqual(ej_u2.ejercicio_19( 490),   "CDXC")
+        self.assertEqual(ej_u2.ejercicio_19( 499), "CDXCIX")
+        self.assertEqual(ej_u2.ejercicio_19( 500),      "D")
+        self.assertEqual(ej_u2.ejercicio_19( 800),   "DCCC")
+        self.assertEqual(ej_u2.ejercicio_19( 900),     "CM")
+        self.assertEqual(ej_u2.ejercicio_19( 990),   "CMXC")
+        self.assertEqual(ej_u2.ejercicio_19( 999), "CMXCIX")
+        self.assertEqual(ej_u2.ejercicio_19(1900),    "MCM")
+        self.assertEqual(ej_u2.ejercicio_19(1500),     "MD")
+        self.assertEqual(ej_u2.ejercicio_19(1000),      "M")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["21", "1"])
+    def test_ejercicio_20_1(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(), "Tu signo es: acuario\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["20", "2"])
+    def test_ejercicio_20_2(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(), "Tu signo es: acuario\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["21", "2"])
+    def test_ejercicio_20_3(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(), "Tu signo es: piscis\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["20", "3"])
+    def test_ejercicio_20_4(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(), "Tu signo es: piscis\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["21", "3"])
+    def test_ejercicio_20_5(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(), "Tu signo es: aries\n")
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @patch("builtins.input", side_effect=["20", "4"])
+    def test_ejercicio_20_6(self, mock_input, mock_stdout):
+        ej_u2.ejercicio_20()
+        self.assertEqual(mock_stdout.getvalue(),"Tu signo es: aries\n")
 
 if __name__ == "__main__":
     unittest.main()
